331afe939271f4abd0cec4a72790255a4ac1fed8
Rodrigo Coutinho <rodrigo.coutinho@locaweb.com.br>
2015-10-05 23:56:05 -0300

    creating empty app

---
+++ src/com/mars_explorer/Location.java
@@ -0,0 +1,18 @@
+package com.mars_explorer;
+
+public class Location {
+  private int x, y;
+  private String direction;
+  //  private Direction direction;
+
+  public Location(int x, int y, String direction){
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  }
+
+  public void Move(String order){
+
+  }
+
+}
---
+++ src/com/mars_explorer/Main.java
@@ -0,0 +1,12 @@
+package com.mars_explorer;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // Simulate an api call 
+    Location location = new Location(0, 0, "N");
+    location.Move("M");
+  }
+
+}
